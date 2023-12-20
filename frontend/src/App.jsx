a83d403e9485080eec4e778053c091bd3036db21
Rose Francis <rosennabs@gmail.com>
2023-12-19 22:56:07 -0700

    Create a single photolistitem component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,24 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
+
+const { id, location, imageSource, username, profile } = sampleDataForPhotoListItem;
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem/>
+      <PhotoListItem key={id} city={location.city} country={location.country} image={imageSource} username={username} profile={profile}/>
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,17 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  return (
+    
+    <div>
+      <img src={props.image} alt="A regular image" />
+      <img src={props.profile} alt="A person's headshot" />
+      <p>{props.username}</p>
+     <span>{props.city} {props.country}</span> 
+    </div>
+   
+  )
 };
 
 export default PhotoListItem;
