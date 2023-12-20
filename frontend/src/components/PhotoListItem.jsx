445d197145c4bd3b927297c9aaec6d2a933b7386
Rose Francis <rosennabs@gmail.com>
2023-12-19 23:49:46 -0700

    Add styling to the PhotoListItem component using Sass

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,11 +6,19 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
   return (
     
-    <div>
-      <img src={props.image} alt="A regular image" />
-      <img src={props.profile} alt="A person's headshot" />
-      <p>{props.username}</p>
-     <span>{props.city} {props.country}</span> 
+    <div className="photo-list__item">
+      <img className="photo-list__image" src={props.image} alt="A regular image" />
+      
+      <div className="photo-list__user-details">
+
+        <div className="photo-list__user-info">
+          <img className="photo-list__user-profile" src={props.profile} alt="A person's headshot" />
+          <p>{props.username}</p>
+          <span className="photo-list__user-location">{props.city} {props.country}</span> 
+        </div>
+        
+      </div>
+      
     </div>
    
   )
