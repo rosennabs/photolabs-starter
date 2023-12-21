ce20c7ccb52973415defada40e5a7d23bd11033d
Rose Francis <rosennabs@gmail.com>
2023-12-21 00:47:05 -0700

    Implement the favourite icon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useReducer} from 'react';
 
 import PhotoListItem from './components/PhotoListItem';
 import PhotoFavButton from 'components/PhotoFavButton';
@@ -18,23 +19,32 @@ const sampleDataForPhotoListItem = {
 const photoArray = new Array(3).fill(sampleDataForPhotoListItem);
 
 
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+
   
   return (
     <div className="App">
-      {photoArray.map((photo, index) => (
-        <PhotoListItem
-          key={index}
-          city={photo.location.city}
-          country={photo.location.country}
-          image={photo.imageSource}
-          username={photo.username}
-          profile={photo.profile} />
+       
+      {photoArray.map((photo) => (
+        
+        <>
+          <PhotoFavButton key={`fav_ ${photo.id}`} />
+          <PhotoListItem
+            key={`list_ ${photo.id}`}
+            city={photo.location.city}
+            country={photo.location.country}
+            image={photo.imageSource}
+            username={photo.username}
+            profile={photo.profile} />
+        </>
         
       ))}
-      <PhotoFavButton/>
+     
     </div>
+
+    
   );
 };
 
--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,11 +3,14 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
+
+
 function PhotoFavButton() {
+  
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        <button></button>
+        <FavIcon />
       </div>
     </div>
   );
